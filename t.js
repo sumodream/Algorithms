3807a158de15eccce69310a784f4b60ff7c6bf2c
褚甜 <sumo_dream@sina.com>
2015-09-04 00:25:28 +0800

    9.3

--- t.js
+++ t.js
@@ -1,3 +1,24 @@
-var a="40";
-var b=7;
-console.log(a%b);
+/**
+ * 去除空格 c
+ */
+
+
+void FormatString(char str[], int len)
+{
+  if(str == NULL || len <= 0)
+  return;
+ 
+  int i = 0, j = 0;
+  while(str[i] == ' ')//开头的空格
+    i++;
+  while(str[i] != '\0')
+  {
+    if(str[i] == ' ' && (str[i+1] == ' ' || str[i+1] == '\0'))//中间或者结尾的空格
+    {
+        i++;
+        continue;
+    }
+    str[j++] = str[i++];
+  }
+  str[j] = '\0';
+}
