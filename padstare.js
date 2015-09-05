fe16ea41a740b411a4a33374b86dd0d30a98700a
褚甜 <sumo_dream@sina.com>
2015-09-05 21:52:18 +0800

    9.5

---
+++ padstare.js
@@ -0,0 +1,22 @@
+/**
+ * [padstare description]
+ *  写一个函数padstare(string str1,min_length,string str2)。
+    例：padstare（‘5’，3，‘0’）返回的是‘005’； padstare（‘798’，3，‘0’）返回的是‘798’；
+ */
+function padstare(str,min,str_char)
+{
+    var ret=[];
+    if(str.length>=min)
+    {
+        return str;  
+    }
+    else
+    {
+        for(var i=0;i<min-str.length;i++)
+          {
+               ret.push(str_char);
+          }
+        return ret.join('')+str;
+    }
+}
+console.log(padstare('5',3,'2'));
