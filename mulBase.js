616ec9f74b542626f17e2433f383b3632f6abda8
sumo <sumo_dream@sina.com>
2015-09-18 11:07:28 +0800

    9.18

---
+++ mulBase.js
@@ -0,0 +1,26 @@
+	/**
+	 * stack
+	 * 2-9进制转换
+	 */
+
+	function mulBase(num , base){
+		var s = [];
+		do{
+			s.push(num % base);
+			num = Math.floor(num /= base);
+		}while(num > 0);
+		var coverted = "";
+		while(s.length>0){
+			coverted += s.pop();
+		}
+		return coverted ;
+	}
+	var num = 32 ;
+	var base = 2 ;
+	var newNum = mulBase(num,base);
+	console.log(num +" coverted to base "+ base +" is "+ newNum);
+	
+	var num = 125 ;
+	var base = 8 ;
+	var newNum = mulBase(num,base);
+	console.log(num +" coverted to base "+ base +" is "+ newNum);
