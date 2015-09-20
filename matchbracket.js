3269f8c3a29bc1c3cece19ffc6491437ddcef483
褚甜 <sumo_dream@sina.com>
2015-09-20 22:04:45 +0800

    9.20

---
+++ matchbracket.js
@@ -0,0 +1,39 @@
+/**
+ * 匹配括号  返回括号缺失的位置
+ */
+function bracket(len){
+    var s = [];
+    for (var index = 0; index < len; index++) {
+            ch = str.charAt(index);
+            switch (ch) {
+            case '{':
+            case '[':
+            case '(':
+                s.push(ch); //如果是前括号，就入栈
+                break;
+            case '}':
+            case ']':
+            case ')':
+                if ( !s) { //是反括号
+                    chPop = s.peek();
+                    if ( ( '{'==chPop && '}' != ch ) //判断栈顶的括号与现在读入的这个反括号是否匹配
+                            ||('['==chPop && ']' != ch )
+                            || ('('==chPop && ')' != ch ) )
+                    {    //不匹配就报错
+                        System.out.println("Error : " + ch + " at " + index);
+                        flag = true;
+                    }else if (( '{'==chPop && '}' == ch ) 
+                            ||('['==chPop && ']' == ch )
+                            || ('('==chPop && ')' == ch )) { 
+                        //匹配就出栈，继续读下一个字符
+                        s.pop();
+                    }
+                } else {//如果遇到反括号，但是栈为空，则出错
+                    System.out.println("Error : " + ch + " at " + index);
+                    flag = true;
+                }
+            default: 
+                break;
+            }
+        }
+    }
--- removenull.js
+++ removenull.js
@@ -1,24 +1,24 @@
-/**
- * 去除空格 c
- */
+  /**
+   * 去除空格 c
+   */
 
 
-void FormatString(char str[], int len)
-{
-  if(str == NULL || len <= 0)
-  return;
- 
-  int i = 0, j = 0;
-  while(str[i] == ' ')//开头的空格
-    i++;
-  while(str[i] != '\0')
+  void FormatString(char str[], int len)
   {
-    if(str[i] == ' ' && (str[i+1] == ' ' || str[i+1] == '\0'))//中间或者结尾的空格
+    if(str == NULL || len <= 0)
+    return;
+   
+    int i = 0, j = 0;
+    while(str[i] == ' ')//开头的空格
+      i++;
+    while(str[i] != '\0')
     {
-        i++;
-        continue;
+      if(str[i] == ' ' && (str[i+1] == ' ' || str[i+1] == '\0'))//中间或者结尾的空格
+      {
+          i++;
+          continue;
+      }
+      str[j++] = str[i++];
     }
-    str[j++] = str[i++];
+    str[j] = '\0';
   }
-  str[j] = '\0';
-}
